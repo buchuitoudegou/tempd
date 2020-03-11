a00ebf82714f9fdd02295b4ecc6aa5bf06376156
buchuitoudegou <756541536@qq.com>
2020-03-11 15:28:28 +0800

    stylize script

---
+++ stylize.py
@@ -0,0 +1,15 @@
+import os
+
+cari_imgs = os.listdir('./style')
+test_imgs = os.listdir('./test_img')
+
+if not os.path.exists('style_result'):
+  os.mkdir('./style_result')
+
+for cari_img in cari_imgs:
+  print(cari_img + 'begin:')
+  for test_img in test_imgs:
+    print(test_img + 'iter...')
+    os.system('python neural_style.py --content {} --styles {} --output {}'\
+      .format('./test_img/' + test_img, './style/' + cari_img, \
+        './style_result/' + test_img.split('.')[0] + '_' + cari_img.split('.')[0] + '.jpg'))
